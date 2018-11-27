d1672b5c222bf3df1008f4caddbfd820e10451d1
Rvithushan <r.vithu0205@gmail.com>
2018-11-27 17:49:17 +0530

    bgimg_file

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,148 @@
+<div id="colorlib-blog">
+			<div class="container">
+				<div class="row">
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img7.jpeg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img2.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">New Saint Michael’s College Residence Hall Buzzing with Student Activity</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img6.jpeg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+					<article class="animate-box">
+						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
+						<div class="entry">
+							<div class="meta text-center">
+								<p>
+									<span>August</span>
+									<span>24, 2017</span>
+								</p>
+							</div>
+							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
+							
+						</div>
+					</article>
+
+				</div>	
+			</div>
+		</div>
+	
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -110,69 +110,8 @@
 		  	</div>
 		</aside>
 
-		<div id="colorlib-blog">
-			<div class="container">
-				<div class="row">
-					<article class="animate-box">
-						<div class="blog-img" style="background-image: url(images/img4.jpg);"></div>
-						<div class="entry">
-							<div class="meta text-center">
-								<p>
-									<span>August</span>
-									<span>24, 2017</span>
-								</p>
-							</div>
-							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
-							<p class="meta-2"><span><i class="icon-user"></i> Admin</span> <span><i class="icon-dropbox"></i> Articles</span></p>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-						</div>
-					</article>
-					<article class="animate-box">
-						<div class="blog-img" style="background-image: url(images/blog-2.jpg);"></div>
-						<div class="entry">
-							<div class="meta text-center">
-								<p>
-									<span>August</span>
-									<span>24, 2017</span>
-								</p>
-							</div>
-							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
-							<p class="meta-2"><span><i class="icon-user"></i> Admin</span> <span><i class="icon-dropbox"></i> Articles</span></p>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						</div>
-					</article>
-					<article class="animate-box">
-						<div class="blog-img" style="background-image: url(images/img2.jpg);"></div>
-						<div class="entry">
-							<div class="meta text-center">
-								<p>
-									<span>August</span>
-									<span>24, 2017</span>
-								</p>
-							</div>
-							<h2><a href="#">New Saint Michael’s College Residence Hall Buzzing with Student Activity</a></h2>
-							<p class="meta-2"><span><i class="icon-user"></i> Admin</span> <span><i class="icon-dropbox"></i> Articles</span></p>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-						</div>
-					</article>
-					<article class="animate-box">
-						<div class="blog-img" style="background-image: url(images/img6.jpeg);"></div>
-						<div class="entry">
-							<div class="meta text-center">
-								<p>
-									<span>August</span>
-									<span>24, 2017</span>
-								</p>
-							</div>
-							<h2><a href="#">Construction was awarded with “The Best Construction Company” prize</a></h2>
-							<p class="meta-2"><span><i class="icon-user"></i> Admin</span> <span><i class="icon-dropbox"></i> Articles</span></p>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-						</div>
-					</article>
-				</div>	
-			</div>
-		</div>
-	
+		@include('gallery')
+		
 		@include('others')
 		
 	</div>
